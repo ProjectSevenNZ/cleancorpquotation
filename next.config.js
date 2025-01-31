c03356e7bb67667480352221d8ca9635fc4662cc
ProjectSevenNZ <delegate@projectseven.co.nz>
2025-01-31 13:12:08 +1300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,11 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
+  
 }
-
+module.exports = {
+  staticPageGenerationTimeout: 60, // Default is 60, try increasing to 120 or more
+};
 module.exports = nextConfig;
 
 // const withFonts = require("next-fonts");
